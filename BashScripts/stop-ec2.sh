855dff430bef2fd69d4acd7f534027f814ccbea7
Chaitanya DevOps <cbingu@gmail.com>
2016-06-13 12:37:34 -0400

    Made some Changes

---
+++ BashScripts/stop-ec2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script to Start EC2 Instances
+# Chaitanya Bingu
+
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+
+/usr/local/bin/ec2-stop-instances i-406947d5 --region us-west-2
+/usr/local/bin/ec2-stop-instances i-4225d4ed --region us-west-2
