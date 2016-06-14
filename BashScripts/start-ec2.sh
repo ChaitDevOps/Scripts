96e71998bd18a3aa21f797c5db6e0b32a60419b2
Chaitanya DevOps <cbingu@gmail.com>
2016-06-14 08:41:45 -0400

    adding ec2 scripts

--- BashScripts/start-ec2.sh
+++ BashScripts/start-ec2.sh
@@ -4,6 +4,7 @@
 
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
+export AWS_CREDENTIAL_FILE
 
 /usr/local/bin/ec2-start-instances i-406947d5 --region us-west-2
 /usr/local/bin/ec2-start-instances i-4225d4ed --region us-west-2
