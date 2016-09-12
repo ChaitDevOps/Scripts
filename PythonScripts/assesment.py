a77ab49bfe6ff9be50c3b6e95f428b6636c389f3
Chaitanya DevOps <cbingu@gmail.com>
2016-09-12 18:59:29 -0400

    adding assesment script

---
+++ PythonScripts/assesment.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# Objects and Data Structures Assessment Test
+# Chait
+# assesment.py
+
+#Numbers : Numeric Data types that store values.
+#Strings : They are sequences in Python.
+#Lists : They are sequences in Python.
+#Tuples : they are just like lists but are immutable. Once a tuple is assigned cannot again be re-assigned.
+#Dictionary : These are mappings in Python. Collections of Objects sorted by a key. Represented Key/Value Pair
+
+# Write an equation that uses multiplication, division, an exponent,
+# addition, and subtraction that is equal to 100.25
+
+print "Everything adds up to:" ,  50*2 - 4 + 2**2 + float(0.5)/2
+print "2 By 3 =", 2/3 # The reason it's zero because in python 2.xx it doesnt do true division.
+# To get true division
+print 2.0/3
+#What is the value of the expression 4 * (6 + 5)
+print 4 * (6 + 5)
+print 4 * 6 + 5
+print 4 + 6 * 5
+#44
+#What is the value of the expression 4 * 6 + 5
+#29
+#What is the value of the expression 4 + 6 * 5
+#34
+print "This float = ",3 + 1.5 + 4
+print "2 Square is =", 2**2
+print "square root of 4 =", 4**1/2
