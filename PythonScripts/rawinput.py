ad22b593c150beac05d7d5a9976a7ecb71419313
Chaitanya DevOps <cbingu@gmail.com>
2017-07-21 15:05:17 -0400

    commit

---
+++ PythonScripts/rawinput.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# Accepting raw input from Keyboard.
+
+from __future__ import print_function
+
+# print("How Old are you?")
+# age = raw_input()
+# print("What's you Name?")
+# name = raw_input()
+# print("You are %s years old" %age)
+# print("Your Name is: %s" %name)
+
+age = raw_input("How Old are you mate?")
+name = raw_input("Whats you name mate?")
+
+print("You are %r years old and your name is %r" %(age,name))
